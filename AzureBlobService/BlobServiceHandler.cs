311a08eff55f4336523cfb87fa50c6348ffedc0c
Muhammad Ali <ali.khan@CooperativeComputing.com>
2024-02-01 21:59:30 +0500

    Add project files.

---
+++ AzureBlobService/BlobServiceHandler.cs
@@ -0,0 +1,161 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using Azure.Storage.Sas;
+
+namespace AzureBlobService
+{
+    public class BlobServiceHandler
+    {
+        private BlobServiceClient _blobServiceClient;
+
+        public BlobServiceHandler(string connectionString)
+        {
+            _blobServiceClient = new BlobServiceClient(connectionString);
+        }
+
+        public async Task CreateContainerAsync(string containerName)
+        {
+            BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            await blobContainerClient.CreateIfNotExistsAsync();
+        }
+
+        public List<string> GetAllContainersName()
+        {
+            Pageable<BlobContainerItem> blobContainerClient = _blobServiceClient.GetBlobContainers();
+            List<string> containersName = new List<string>();
+
+            foreach (BlobContainerItem item in blobContainerClient)
+            {
+                containersName.Add(item.Name);
+            }
+
+            return containersName;
+        }
+
+        public async Task UploadBlobAsync(string containerName, string blobName, string sourcePath)
+        {
+            await CreateContainerAsync(containerName);
+            BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+            await blobClient.UploadAsync(sourcePath);
+        }
+
+        public List<string> GetAllBlobsName(string containerName)
+        {
+            BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            List<string> blobsName = new List<string>();
+            
+            foreach(BlobItem item in blobContainerClient.GetBlobs())
+            {
+                blobsName.Add(item.Name);
+            }
+
+            return blobsName;
+        }
+
+        public async Task DownloadToAsync(string containerName, string blobName, string destinationPath)
+        {
+            BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+            await blobClient.DownloadToAsync(destinationPath);
+        }
+
+        public async Task DeleteIfExistsAsync(string containerName, string blobName)
+        {
+            BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+            await blobClient.DeleteIfExistsAsync();
+        }
+
+        public async Task<string> GetBlobUrl(string containerName, string blobName, TimeSpan? sasExpiresOn)
+        {
+            string url = string.Empty;
+
+            if(sasExpiresOn is null)
+            {
+                url = @$"{_blobServiceClient.Uri}{containerName}/{blobName}";
+            }
+            else
+            {
+                BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+                BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+
+                BlobSasBuilder builder = new BlobSasBuilder()
+                {
+                    BlobContainerName = containerName,
+                    BlobName = blobName,
+                    Resource = "b",
+                    ExpiresOn = DateTime.UtcNow.AddSeconds(sasExpiresOn.Value.TotalSeconds),
+                };
+
+                builder.SetPermissions(BlobSasPermissions.Read | BlobSasPermissions.List);
+
+                Uri blobURI = blobClient.GenerateSasUri(builder);
+
+                url = blobURI.AbsoluteUri;
+            }
+
+            return url;
+        }
+
+        public async Task<Dictionary<string, string>> GetBlobPropertiesAsync(string containerName, string blobName)
+        {
+            BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+            BlobProperties blobProperties = await blobClient.GetPropertiesAsync();
+            
+            Dictionary<string, string> properties = new Dictionary<string, string>
+            {
+                { "AccessTier", blobProperties.AccessTier.ToString() },
+                { "ContentLength", blobProperties.ContentLength.ToString() },
+                { "ContentType", blobProperties.ContentType.ToString() },
+                { "LastAccessed", blobProperties.LastAccessed.ToString() },
+                { "LastModified", blobProperties.LastModified.ToString() },
+                { "LeaseDuration", blobProperties.LeaseDuration.ToString() }
+            };
+
+            foreach(KeyValuePair<string, string> metadata in blobProperties.Metadata)
+            {
+                properties.Add(metadata.Key, metadata.Value);
+            }
+
+            return properties;
+        }
+
+        public async Task SetBlobMatadataAsync(string containerName, string blobName, Dictionary<string, string> metadata)
+        {
+            BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+            BlobProperties blobProperties = await blobClient.GetPropertiesAsync();
+
+            foreach (KeyValuePair<string, string> metadataItem in metadata)
+            {
+                blobProperties.Metadata.TryAdd(metadataItem.Key, metadataItem.Value);
+            }
+
+            await blobClient.SetMetadataAsync(blobProperties.Metadata);
+        }
+
+        public async Task UpdateBlobAsync(string containerName, string blobName, string sourcePath)
+        {
+            BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+
+            BlobLeaseClient blobLeaseClient = blobClient.GetBlobLeaseClient();
+            BlobLease blobLease = await blobLeaseClient.AcquireAsync(TimeSpan.FromSeconds(60));
+
+            BlobUploadOptions blobUploadOptions = new()
+            {
+                Conditions = new()
+                {
+                    LeaseId = blobLease.LeaseId,
+                }
+            };
+
+            await blobClient.UploadAsync(sourcePath, blobUploadOptions);
+            await blobLeaseClient.ReleaseAsync();
+        }
+    }
+}
---
+++ AzureBlobService/Program.cs
@@ -0,0 +1,24 @@
+﻿using AzureBlobService;
+
+string connectionString = "";
+BlobServiceHandler blobServiceHandler = new BlobServiceHandler(connectionString);
+
+//await blobServiceHandler.CreateContainerAsync("dotnet-program");
+//blobServiceHandler.GetAllContainersName().ForEach(x => Console.WriteLine(x));
+//await blobServiceHandler.UploadBlobAsync("dotnet-program", $"2024/01/30/{Guid.NewGuid()}", @"C:\Users\CC378\Downloads\TestLogo2.jpg");
+//await blobServiceHandler.DownloadToAsync("dotnet-program", $"2024/01/30/4bdc97cf-eaca-45e8-8bb3-a07922bd80d6", @"C:\Users\CC378\Downloads\TestLogo222.jpg");
+//await blobServiceHandler.DeleteIfExistsAsync("dotnet-program", $"2024/01/30/4bdc97cf-eaca-45e8-8bb3-a07922bd80d6");
+//string url = await blobServiceHandler.GetBlobUrl("dotnet-program", $"2024/01/30/6ead8bb8-70e2-45e3-b108-c08a54280a9a", TimeSpan.FromSeconds(120)); Console.WriteLine(url);
+//blobServiceHandler.GetAllBlobsName("dotnet-program").ForEach(x => Console.WriteLine(x));
+
+//Dictionary<string, string> blobProperties = await blobServiceHandler.GetBlobPropertiesAsync("dotnet-program", $"2024/01/30/0a2895d3-7b67-4eb3-982b-f0750261130a");
+//foreach (var property in blobProperties)
+//    Console.WriteLine($"{property.Key}: {property.Value}");
+
+//await blobServiceHandler.SetBlobMatadataAsync("dotnet-program", $"2024/01/30/0a2895d3-7b67-4eb3-982b-f0750261130a", new Dictionary<string, string> { { "FirstKey", "FirstValue" }, { "SecondKey", "SecondValue" } });
+
+//await blobServiceHandler.UpdateBlobAsync("dotnet-program", $"2024/01/30/0a2895d3-7b67-4eb3-982b-f0750261130a", @"C:\Users\CC378\Downloads\TestLogo.jpg");
+
+
+
+Console.WriteLine("Hello, World!");
